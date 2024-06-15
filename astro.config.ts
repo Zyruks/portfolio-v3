18058ce9ed787c08afde1b0da2fd9813c24b8ee7
Samuel Llibre-Pillco <dev@zyruks.com>
2024-06-15 14:44:13 -0400

    chore(config): 🔧 update astro, lint-staged config

--- astro.config.ts
+++ astro.config.ts
@@ -4,12 +4,8 @@ import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
-  vite: {
-    css: {
-      transformer: 'lightningcss',
-    },
-  },
+
   integrations: [tailwind()],
 });
